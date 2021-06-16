f1794d6f05cfd4c03554a982a5d97a2ad9d684e5
Wuttipat <wuttipat@outlook.com>
2021-06-17 05:18:43 +0700

    Design store, reducer and action for redux

---
+++ src/js/store/index.js
@@ -0,0 +1,16 @@
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from '../reducers';
+import { fetchProducts } from '../actions';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+	reducers,
+	composeEnhancers(applyMiddleware(thunk))
+);
+
+store.dispatch(fetchProducts());
+
+export default store;
